d514ed87be62cc07a334461e9eb6d9aaed7647d0
Alexandr <kino_mac@mail.ru>
2022-02-17 13:46:00 +0300

    Добавлено решение 3-ей задачи

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 38: Найти сумму чисел одномерного массива, стоящих на нечетной позиции.
+
+int[] GetArray(int sizeArray)  // Задаем массив из случайных целых чисел в диапазоне [-100;100] и выводим массив на экран
+{
+    Random randomizer = new Random();
+    Console.Write("Исходный массив: ");
+    int[] newArray = new int[sizeArray];
+    for (int i = 0; i < sizeArray; i++)
+    {
+        newArray[i] = randomizer.Next(-100,101);
+        Console.Write($"{newArray[i]}  ");
+    }
+    Console.WriteLine();
+    return newArray;
+}
+
+int[] newArray = GetArray(11);
+int sum = 0;
+for (int i = 1; i < newArray.Length; i += 2)
+{
+    sum = sum + newArray[i];
+}
+Console.WriteLine($"Сумма чисел массива, стоящих на нечетной позиции: {sum}");
