1921bab5a16d467a4928f2e9a44f2fabd02fd49f
Alexandr <kino_mac@mail.ru>
2022-02-17 13:15:34 +0300

    Добавлено решение 2-ой задачи

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 36: Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел.
+
+int[] GetArray() // Задаем массив из 10 положительных трехзначных чисел
+{
+    Random randomizer = new Random();
+    Console.Write("Исходный массив: ");
+    int[] newArray = new int[10];
+    for (int i = 0; i < 10; i++)
+    {
+        newArray[i] = randomizer.Next(100,1000);
+        Console.Write($"{newArray[i]}   ");
+    }
+    Console.WriteLine();
+    return newArray;
+}
+
+int[] newArray = GetArray();
+int countEven = 0;
+int countNotEven = 0;
+foreach (int item in newArray)
+{
+    if (item % 2 == 0)
+        countEven++;
+    else
+        countNotEven++;
+}
+Console.WriteLine($"Количество нечетных чисел в массиве - {countNotEven}, четных - {countEven}");
